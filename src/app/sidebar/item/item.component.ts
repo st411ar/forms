3ae2feb72dd3934c7de476c24ffd4a69f464db85
Vitaly Kiselev <st411ar@ya.ru>
2018-09-10 16:10:58 +0300

    implemented sidebar item label

--- src/app/sidebar/item/item.component.ts
+++ src/app/sidebar/item/item.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { ExampleModel } from '../../models/example.model';
+
 
 @Component({
 	selector: 'app-item',
 	templateUrl: './item.component.html'
 })
 export class ItemComponent implements OnInit {
+	@Input() item: ExampleModel;
 
 	constructor() {}
 
