b9012ad3379899dcd89fee0308ffbebe94a42821
Vitaly Kiselev <st411ar@ya.ru>
2018-09-10 15:35:43 +0300

    created example model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+
+import { ExampleModel } from './models/example.model';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms';
+	constructor(@Inject('Examples') examples: ExampleModel[]) {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { ExampleModel } from './models/example.model';
+
+
+export const examples: ExampleModel[] = [ 
+	{ label: 'Intro' }
+];
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule
+	],
+	providers: [
+		{ provide: 'Examples', useValue: examples }
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
