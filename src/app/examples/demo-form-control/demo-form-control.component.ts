d203d1d9181c928edb2b5cf6776be80aacf83a13
Vitaly Kiselev <st411ar@ya.ru>
2018-09-10 17:24:19 +0300

    created demo form control component

---
+++ src/app/examples/demo-form-control/demo-form-control.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-demo-form-control',
+	templateUrl: './demo-form-control.component.html'
+})
+export class DemoFormControlComponent implements OnInit {
+
+	constructor() {}
+
+	ngOnInit() {}
+
+}
