822e440e5b62ec61edffb35b7cfcdc2a2dafbe0d
Vitaly Kiselev <st411ar@ya.ru>
2018-09-11 13:20:04 +0300

    completed explicit validations component

--- src/app/examples/demo-form-with-validations-explicit/demo-form-with-validations-explicit.component.ts
+++ src/app/examples/demo-form-with-validations-explicit/demo-form-with-validations-explicit.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-demo-form-with-validations-explicit',
 	templateUrl: './demo-form-with-validations-explicit.component.html'
 })
 export class DemoFormWithValidationsExplicitComponent implements OnInit {
+	myForm: FormGroup;
+	sku: AbstractControl;
 
-	constructor() {}
+	constructor(fb: FormBuilder) {
+		this.myForm = fb.group({
+			'sku': [``, Validators.required]
+		});
+
+		this.sku = this.myForm.controls['sku'];
+	}
 
 	ngOnInit() {}
+
+
+	onSubmit(value: string): void {
+		console.log(`you submitted value: `, value);
+	}
 
 }
