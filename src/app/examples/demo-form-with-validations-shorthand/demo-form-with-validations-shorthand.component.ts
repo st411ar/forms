5270722dc7e47d15d458627fa75934daa717ccb0
Vitaly Kiselev <st411ar@ya.ru>
2018-09-11 14:10:31 +0300

    created validations shorthand component

---
+++ src/app/examples/demo-form-with-validations-shorthand/demo-form-with-validations-shorthand.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-demo-form-with-validations-shorthand',
+	templateUrl: './demo-form-with-validations-shorthand.component.html',
+})
+export class DemoFormWithValidationsShorthandComponent implements OnInit {
+
+	constructor() {}
+
+	ngOnInit() {}
+
+}
