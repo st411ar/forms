82aff00669163698d60c2bc5832528d1974c6400
Vitaly Kiselev <st411ar@ya.ru>
2018-09-11 14:18:52 +0300

    completed validations shorthand component

--- src/app/examples/demo-form-with-validations-shorthand/demo-form-with-validations-shorthand.component.ts
+++ src/app/examples/demo-form-with-validations-shorthand/demo-form-with-validations-shorthand.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-demo-form-with-validations-shorthand',
 	templateUrl: './demo-form-with-validations-shorthand.component.html',
 })
 export class DemoFormWithValidationsShorthandComponent implements OnInit {
+	myForm: FormGroup;
 
-	constructor() {}
+	constructor(fb: FormBuilder) {
+		this.myForm = fb.group({
+			'sku': [``, Validators.required]
+		});
+	}
 
 	ngOnInit() {}
+
+
+	onSubmit(value: string): void {
+		console.log(`you submitted value: `, value);
+	}
 
 }
