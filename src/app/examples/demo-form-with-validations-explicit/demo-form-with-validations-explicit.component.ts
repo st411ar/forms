7281faba946dc3824cc7399a9638557b3977b74b
Vitaly Kiselev <st411ar@ya.ru>
2018-09-11 12:57:35 +0300

    created explicit validations component

---
+++ src/app/examples/demo-form-with-validations-explicit/demo-form-with-validations-explicit.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-demo-form-with-validations-explicit',
+	templateUrl: './demo-form-with-validations-explicit.component.html'
+})
+export class DemoFormWithValidationsExplicitComponent implements OnInit {
+
+	constructor() {}
+
+	ngOnInit() {}
+
+}
