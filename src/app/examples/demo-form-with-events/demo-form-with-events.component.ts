4e24c1e5e92cc09fa8642d1c845bfdb9d88a1e29
Vitaly Kiselev <st411ar@ya.ru>
2018-09-11 16:13:03 +0300

    created form with events component

---
+++ src/app/examples/demo-form-with-events/demo-form-with-events.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-demo-form-with-events',
+	templateUrl: './demo-form-with-events.component.html'
+})
+export class DemoFormWithEventsComponent implements OnInit {
+
+	constructor() {}
+
+	ngOnInit() {}
+
+}
