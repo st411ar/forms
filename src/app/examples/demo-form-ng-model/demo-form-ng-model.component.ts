968f6008581d99b018c9ae646b2a7af4dbe104f3
Vitaly Kiselev <st411ar@ya.ru>
2018-09-11 17:43:56 +0300

    completed form ng-model component

--- src/app/examples/demo-form-ng-model/demo-form-ng-model.component.ts
+++ src/app/examples/demo-form-ng-model/demo-form-ng-model.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-demo-form-ng-model',
 	templateUrl: './demo-form-ng-model.component.html'
 })
 export class DemoFormNgModelComponent implements OnInit {
+	myForm: FormGroup;
+	productName: string;
 
-	constructor() {}
+	constructor(fb: FormBuilder) {
+		this.myForm = fb.group({
+			'productName': [``, Validators.required]
+		});
+	}
 
 	ngOnInit() {}
+
+
+	onSubmit(value: any): void {
+		console.log(`you submitted value: `, value);
+	}
 
 }
