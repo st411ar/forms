f2778979ebbd8b7186ab79bcca9f4ad457ec9508
Vitaly Kiselev <st411ar@ya.ru>
2018-09-11 16:49:26 +0300

    completed form with events componentn

--- src/app/examples/demo-form-with-events/demo-form-with-events.component.ts
+++ src/app/examples/demo-form-with-events/demo-form-with-events.component.ts
@@ -1,13 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
 	selector: 'app-demo-form-with-events',
 	templateUrl: './demo-form-with-events.component.html'
 })
 export class DemoFormWithEventsComponent implements OnInit {
+	myForm: FormGroup;
+	sku: AbstractControl;
 
-	constructor() {}
+	constructor(fb: FormBuilder) {
+		this.myForm = fb.group({
+			'sku': [``, Validators.required]
+		});
+
+		this.sku = this.myForm.controls['sku'];
+
+		this.sku.valueChanges.subscribe(
+			(value: string) => {
+				console.log(`sku changed to: `, value);
+			}
+		);
+
+		this.myForm.valueChanges.subscribe(
+			(form: any) => {
+				console.log(`form changed to: `, form);
+			}
+		);
+	}
 
 	ngOnInit() {}
+
+
+	onSubmit(value: string): void {
+		console.log(`you submitted value: `, value);
+	}
 
 }
